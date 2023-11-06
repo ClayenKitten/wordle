b0b491f7bc0dd92eb5bd8fcd2f567477ba52bde5
ClayenKitten <dima070704@yandex.ru>
2023-11-06 13:06:38 +0300

    Add `Icon` utility component

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let name: "chart" | "gear";
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src={`icons/${name}.svg`} />
+
+<style lang="scss">
+    img {
+        filter: invert(1);
+        height: var(--icon-size, 32px);
+        width: var(--icon-size, 32px);
+    }
+</style>
